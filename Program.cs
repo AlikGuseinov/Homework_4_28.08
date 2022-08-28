217a5c1f64a5ed2efaea4a813c9a0df315561d81
Alik <9774952689@bk.ru>
2022-08-28 22:58:47 +0300

    Задача №4

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+     if(a == b)
+    Console.WriteLine("Вы ввели одинаковые числа, попробуйте ещё раз)");
+    if(c == b)
+    Console.WriteLine("Вы ввели одинаковые числа, попробуйте ещё раз)");
+    if(a == c)
+    Console.WriteLine("Вы ввели одинаковые числа, попробуйте ещё раз)");
+        if(b > max)
+         {
+         max = b;
+         if (c >max)
+         max = c;
+
+        Console.WriteLine("Максимальное число: " + max);
+    
+  
+         }
